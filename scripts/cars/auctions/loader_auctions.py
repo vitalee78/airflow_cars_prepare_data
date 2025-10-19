4dfc794acbfc4c45c44283f5aba1750128fef0c3
vitalee <vitalee78@yandex.ru>
2025-10-19 11:54:51 +0700

    logging.error(f"Database error: {e}") added for full method

--- scripts/cars/auctions/loader_auctions.py
+++ scripts/cars/auctions/loader_auctions.py
@@ -16,43 +16,43 @@ class LoaderAuctions:
         self.airflow_mode = airflow_mode
 
     def save_auctions_to_db(self, df: pd.DataFrame):
-        if df.empty:
-            return
+        try:
+            if df.empty:
+                return
 
-        ref = ReferenceLoader(airflow_mode=self.airflow_mode)
-        engine = get_engine(airflow_mode=self.airflow_mode)
+            ref = ReferenceLoader(airflow_mode=self.airflow_mode)
+            engine = get_engine(airflow_mode=self.airflow_mode)
 
-        # Подготавливаем данные
-        records = []
-        for _, row in df.iterrows():
-            brand_id = ref.get_or_create_brand(row['brand'])
-            model_id = ref.get_or_create_model(brand_id, row['model'])
-            carbody_id = ref.get_or_create_carbody(model_id, row.get('carbody'))
+            # Подготавливаем данные
+            records = []
+            for _, row in df.iterrows():
+                brand_id = ref.get_or_create_brand(row['brand'])
+                model_id = ref.get_or_create_model(brand_id, row['model'])
+                carbody_id = ref.get_or_create_carbody(model_id, row.get('carbody'))
 
-            records.append({
-                "id_brand": brand_id,
-                "id_model": model_id,
-                "id_carbody": carbody_id,
-                "id_car": row['id_car'],
-                "equipment": row.get('equipment'),
-                "year_release": row['year'],
-                "mileage": row.get('mileage'),
-                "source_lot_id": row['source_lot_id'],
-                "link_source": row['link_source'],
-                "auction_date": str(row['lot_date']),
-                "rate": row.get('rate')
-            })
+                records.append({
+                    "id_brand": brand_id,
+                    "id_model": model_id,
+                    "id_carbody": carbody_id,
+                    "id_car": row['id_car'],
+                    "equipment": row.get('equipment'),
+                    "year_release": row['year'],
+                    "mileage": row.get('mileage'),
+                    "source_lot_id": row['source_lot_id'],
+                    "link_source": row['link_source'],
+                    "auction_date": str(row['lot_date']),
+                    "rate": row.get('rate')
+                })
 
-        json_str = json.dumps(records, ensure_ascii=False)
+            json_str = json.dumps(records, ensure_ascii=False)
 
-        check_isvalid_json(json_str)
+            check_isvalid_json(json_str)
 
-        with engine.begin() as conn:
-            try:
+            with engine.begin() as conn:
                 conn.execute(
                     text("SELECT public.fn_upsert_auction_cars(:data)"),
                     {"data": json_str}
                 )
-            except Exception as e:
-                logging.error(f"Database error: {e}")
-                sys.exit(1)
+        except Exception as e:
+            logging.error(f"Database error: {e}")
+            sys.exit(1)
--- scripts/cars/lots/loader_lots.py
+++ scripts/cars/lots/loader_lots.py
@@ -16,46 +16,44 @@ class LotsLoader:
         self.airflow_mode = airflow_mode
 
     def save_lots_to_db(self, df: pd.DataFrame):
-        if df.empty:
-            return
+        try:
+            if df.empty:
+                return
 
-        ref = ReferenceLoader(airflow_mode=self.airflow_mode)
-        engine = get_engine(airflow_mode=self.airflow_mode)
+            ref = ReferenceLoader(airflow_mode=self.airflow_mode)
+            engine = get_engine(airflow_mode=self.airflow_mode)
 
-        records = []
-        for _, row in df.iterrows():
-            # 1. Получаем или создаём справочники
-            brand_id = ref.get_or_create_brand(row['brand'])
-            model_id = ref.get_or_create_model(brand_id, row['model'])
-            carbody_id = ref.get_or_create_carbody(model_id, row.get('carbody'))
+            records = []
+            for _, row in df.iterrows():
+                # 1. Получаем или создаём справочники
+                brand_id = ref.get_or_create_brand(row['brand'])
+                model_id = ref.get_or_create_model(brand_id, row['model'])
+                carbody_id = ref.get_or_create_carbody(model_id, row.get('carbody'))
 
-            records.append({
-                "id_brand": brand_id,
-                "id_model": model_id,
-                "id_carbody": carbody_id,
-                "id_car": row['id_car'],
-                "cost": row.get('cost'),
-                "year_release": row['year'],
-                "rate": row.get('rate'),
-                "mileage": row.get('mileage'),
-                "source_lot_id": row['source_lot_id'],
-                "link_source": row['link_source'],
-                "auction_date": str(row['lot_date']),
-                "equipment": row.get('equipment')
-            })
+                records.append({
+                    "id_brand": brand_id,
+                    "id_model": model_id,
+                    "id_carbody": carbody_id,
+                    "id_car": row['id_car'],
+                    "cost": row.get('cost'),
+                    "year_release": row['year'],
+                    "rate": row.get('rate'),
+                    "mileage": row.get('mileage'),
+                    "source_lot_id": row['source_lot_id'],
+                    "link_source": row['link_source'],
+                    "auction_date": str(row['lot_date']),
+                    "equipment": row.get('equipment')
+                })
 
-            json_str = json.dumps(records, ensure_ascii=False)
+                json_str = json.dumps(records, ensure_ascii=False)
 
-            check_isvalid_json(json_str)
+                check_isvalid_json(json_str)
 
-            with engine.begin() as conn:
-                try:
+                with engine.begin() as conn:
                     conn.execute(
                         text("SELECT public.fn_upsert_lots_cars(:data)"),
                         {"data": json_str}
                     )
-                except Exception as e:
-                    logging.error(f"Database error: {e}")
-                    sys.exit(1)
-
-
+        except Exception as e:
+            logging.error(f"Database error: {e}")
+            sys.exit(1)
